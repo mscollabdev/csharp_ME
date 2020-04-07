87aa3d1fe15208c3bf46c238796c161b8ccf646f
Akshay <akshay@mscollabdev.onmicrosoft.com>
2020-04-07 03:48:59 +0000

    changes in controller code

--- Bots/TeamsMessagingExtensionsSearchBot.cs
+++ Bots/TeamsMessagingExtensionsSearchBot.cs
@@ -101,24 +101,4 @@ namespace Microsoft.BotBuilderSamples.Bots
         }
     }
 
-    public class EchoBot : ActivityHandler
-    {
-        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
-        {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
-        }
-
-        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
-        {
-            var welcomeText = "Hello and welcome!";
-            foreach (var member in membersAdded)
-            {
-                if (member.Id != turnContext.Activity.Recipient.Id)
-                {
-                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-                }
-            }
-        }
-    }
 }
